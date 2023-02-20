198f8ffc0f783dcfe23b9e0ad86cdb256559454a
ayedun <88404572+ayedun@users.noreply.github.com>
2023-02-19 18:15:34 -0800

    Add row/column testing

--- lib/game.rb
+++ lib/game.rb
@@ -3,13 +3,14 @@ class Game
         @player1 = player1
         @player2 = player2
         @board = Array.new(7, " ") {Array.new(6, " ")}
+        @winner = nil
         #@current_player = @player1
     end
 
     def place_token(player, column)
         bottom_most = get_bottom(column)
-        p bottom_most
         @board[bottom_most][column] = player.symbol
+        check_win
         # p get_bottom(column)
     end
     def board
@@ -27,15 +28,49 @@ class Game
 
     def check_win
         #check horizontal
-        @board.each_cons(4) do |a, b, c, d|
-            if a == b && b == c && c == d && a != " "
-                return true
+        @board.each do |row|
+            # p "This is a row #{row}"
+            row.each_cons(4) do |a, b, c, d|
+                if a == b && b == c && c == d && a !=(" ")
+                    if a == @player1.symbol
+                        select_winner(@player1)
+                    else
+                        select_winner(@player2)
+                    end
+                    return true
+                end
             end
         end
+ 
 
         #check vertical
+        @board.transpose.each do |column|
+            column.each_cons(4) do |a, b, c, d|
+                if a == b && b == c && c == d && a !=(" ")
+                    if a == @player1.symbol
+                        select_winner(@player1)
+                    else
+                        select_winner(@player2)
+                    end
+                    return true
+                end
+            end
+            
+        end
         #check diagonal
+
+        return false
     end
+
+    def select_winner(player)
+        @winner = player
+        "Congrats #{player.name}. You won!}"
+    end
+
+    def return_winner
+        @winner
+    end
+
 
     def print_board
         @board.reverse.each do |row|
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,18 +12,26 @@ describe Game do
         let (:column3) {3}
         let (:column4) {4}
         let (:column5) {5}
+        let (:column2) {2}
+
         let (:row) {2}
+    
         it "should land on the bottom of the chosen column" do
             game.place_token(player1, column)
             expect(game.board[0][column]).to eq(player1.symbol)
             game.print_board
+            expect(game.check_win).to eq(false)
+
         end
         it "should land at the bottom-most of the chosen column(twice)" do
             game.place_token(player1, column)
             expect(game.board[0][column]).to eq(player1.symbol)
             game.place_token(player1, column)
             expect(game.board[1][column]).to eq(player1.symbol)
+
             game.print_board
+            expect(game.check_win).to eq(false)
+
         end
         it "should land at the bottom-most of the chosen column(p1, p2, p1)" do
             game.place_token(player1,column)
@@ -33,8 +41,19 @@ describe Game do
             game.place_token(player1,column)
             expect(game.board[2][column]).to eq(player1.symbol)
             game.print_board
-        end
+            expect(game.check_win).to eq(false)
 
+        end
+    end
+
+    context "Row-based wins" do
+        let (:row) {2}
+        let (:column2) {2}
+        let (:column) {3}
+        let (:column3) {3}
+        let (:column4) {4}
+        let (:column5) {5}
+        #row-focused tests
         it "p1 has 4 in a row" do
             
             game.place_token(player1,column3)
@@ -45,9 +64,145 @@ describe Game do
             
             game.place_token(player1,column5)
             expect(game.board[0][column5]).to eq(player1.symbol)
+            
+            game.place_token(player1,column2)
+            expect(game.board[0][column2]).to eq(player1.symbol)
             game.print_board
+
+            expect(game.check_win).to eq(true)
+            expect(game.return_winner).to eq(player1)
+
         end
-    
+
+        it "p2 has 4 in a row" do
+            
+            game.place_token(player2,column3)
+            expect(game.board[0][column3]).to eq(player2.symbol)
+            
+            game.place_token(player2,column4)
+            expect(game.board[0][column4]).to eq(player2.symbol)
+            
+            game.place_token(player2,column5)
+            expect(game.board[0][column5]).to eq(player2.symbol)
+            
+            game.place_token(player2,column2)
+            expect(game.board[0][column2]).to eq(player2.symbol)
+            game.print_board
+
+            expect(game.check_win).to eq(true)
+            expect(game.return_winner).to eq(player2)
+
+        end
+
+        it "pattern is p1,p1,p2,p1 in a row" do
+            
+            game.place_token(player1,column2)
+            expect(game.board[0][column2]).to eq(player1.symbol)
+            
+            game.place_token(player1,column3)
+            expect(game.board[0][column3]).to eq(player1.symbol)
+            
+            game.place_token(player2,column4)
+            expect(game.board[0][column4]).to eq(player2.symbol)
+            
+            game.place_token(player1,column5)
+            expect(game.board[0][column5]).to eq(player1.symbol)
+            game.print_board
+
+            expect(game.check_win).to eq(false)
+            expect(game.return_winner).to be_nil
+
+
+        end
+    end
+
+    context "Column-based wins" do
+        let (:row0) {0}
+        let (:row1) {1}
+        let (:row2) {2}
+        let (:row3) {3}
+        let (:column2) {2}
+        let (:column3) {3}
+        let (:column4) {4}
+
+        it "p1 has 4 in a column" do
+            
+            game.place_token(player1,column2)
+            expect(game.board[row0][column2]).to eq(player1.symbol)
+            
+            game.place_token(player1,column2)
+            expect(game.board[row1][column2]).to eq(player1.symbol)
+            
+            game.place_token(player1,column2)
+            expect(game.board[row2][column2]).to eq(player1.symbol)
+            
+            game.place_token(player1,column2)
+            expect(game.board[row3][column2]).to eq(player1.symbol)
+            game.print_board
+
+            expect(game.check_win).to eq(true)
+            expect(game.return_winner).to eq(player1)
+
+        end
+
+        it "p2 has 4 in a column" do
+            
+            game.place_token(player2,column2)
+            expect(game.board[row0][column2]).to eq(player2.symbol)
+            
+            game.place_token(player2,column2)
+            expect(game.board[row1][column2]).to eq(player2.symbol)
+            
+            game.place_token(player2,column2)
+            expect(game.board[row2][column2]).to eq(player2.symbol)
+            
+            game.place_token(player2,column2)
+            expect(game.board[row3][column2]).to eq(player2.symbol)
+            game.print_board
+
+            expect(game.check_win).to eq(true)
+            expect(game.return_winner).to eq(player2)
+
+        end
+
+        it "pattern is p1,p1,p2,p1 in a column" do
+            
+            game.place_token(player1,column2)
+            expect(game.board[0][column2]).to eq(player1.symbol)
+            
+            game.place_token(player1,column2)
+            expect(game.board[1][column2]).to eq(player1.symbol)
+            
+            game.place_token(player2,column2)
+            expect(game.board[2][column2]).to eq(player2.symbol)
+            
+            game.place_token(player1,column2)
+            expect(game.board[3][column2]).to eq(player1.symbol)
+            game.print_board
+
+            expect(game.check_win).to eq(false)
+            expect(game.return_winner).to be_nil
+
+
+        end
+    end
+
+    context "Diagonal-based wins" do
+        let (:row0) {0}
+        let (:row1) {1}
+        let (:row2) {2}
+        let (:row3) {3}
+        let (:row4) {4}
+        let (:row5) {5}
+        let (:column0) {0}
+        let (:column1) {1}
+        let (:column2) {2}
+        let (:column3) {3}
+        let (:column4) {4}        
+        let (:column5) {5}
+
+        
+
     end
     
 end
