9b6c33135ad9fb993ffd2c5e09aff7695801ece0
ayedun <88404572+ayedun@users.noreply.github.com>
2023-02-20 19:43:01 -0800

    Add diagonal testing

--- lib/game.rb
+++ lib/game.rb
@@ -58,7 +58,69 @@ class Game
             
         end
         #check diagonal
+        #+1,+1 loop
+        column_index= 0
 
+        @board.transpose.each do |column|
+            column.each_with_index do |a, row|
+
+                #Check if the next row in the diagonal is valid before checking their value.
+                if !(@board[row])|| !(@board[row+1])|| !(@board[row+2]) == nil || !(@board[row+3])
+                    next
+                end
+
+                #Check if the next column in the diagonal is valid before checking their value.
+
+                if @board[row][column_index] == nil|| @board[row+1][column_index+1] == nil || @board[row+2][column_index+2] == nil || @board[row+3][column_index+3] == nil
+                    next
+                end
+                #p "diagonal #{@board[row][column_index]}, #{@board[row+1][column_index+1]}, #{@board[row+2][column_index+2]}, #{@board[row+3][column_index+3]}"
+
+                #If the next 3 rows and columns are valid, check if they are equal to the current row and column.
+                if @board[row][column_index] == @board[row+1][column_index+1] && @board[row+1][column_index+1] == @board[row+2][column_index+2] && @board[row+2][column_index+2] == @board[row+3][column_index+3] && @board[row][column_index] !=(" ") 
+                    if @board[row][column_index] == @player1.symbol
+                        select_winner(@player1)
+                    else
+                        select_winner(@player2)
+                    end
+                   
+                    return true
+                end
+            end
+            column_index= column_index + 1
+        end
+        #+1,-1 loop
+        column_index= 0
+        @board.transpose.each do |column|
+            
+            column.each_with_index do |a, row|
+                #Check if the next row in the diagonal is valid before checking their value.
+                if !(@board[row])|| !(@board[row-1])|| !(@board[row-2]) == nil || !(@board[row-3])
+                    next
+                end
+
+                #Check if the next column in the diagonal is valid before checking their value.
+
+                if @board[row][column_index] == nil|| @board[row-1][column_index+1] == nil || @board[row-2][column_index+2] == nil || @board[row-3][column_index+3] == nil
+                    next
+                end
+                #If the next 3 rows and columns are valid, check if they are equal to the current row and column.
+                if @board[row][column_index] == @board[row-1][column_index+1] && @board[row-1][column_index+1] == @board[row-2][column_index+2] && @board[row-2][column_index+2] == @board[row-3][column_index+3] && @board[row][column_index] !=(" ") 
+                    if @board[row][column_index] == @player1.symbol
+                        select_winner(@player1)
+                    else
+                        select_winner(@player2)
+                    end
+                    return true
+                end
+            end
+            column_index= column_index + 1
+        end
+
+        
+
+
+        #If no winner, return false
         return false
     end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -55,18 +55,26 @@ describe Game do
         let (:column5) {5}
         #row-focused tests
         it "p1 has 4 in a row" do
-            
-            game.place_token(player1,column3)
-            expect(game.board[0][column3]).to eq(player1.symbol)
-            
+            game.place_token(player1,column2)
+            expect(game.board[0][column2]).to eq(player1.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[0][column3]).to eq(player2.symbol)
             game.place_token(player1,column4)
             expect(game.board[0][column4]).to eq(player1.symbol)
-            
             game.place_token(player1,column5)
             expect(game.board[0][column5]).to eq(player1.symbol)
             
+            game.place_token(player1,column3)
+            expect(game.board[1][column3]).to eq(player1.symbol)
+            
+            game.place_token(player1,column4)
+            expect(game.board[1][column4]).to eq(player1.symbol)
+            
+            game.place_token(player1,column5)
+            expect(game.board[1][column5]).to eq(player1.symbol)
+            
             game.place_token(player1,column2)
-            expect(game.board[0][column2]).to eq(player1.symbol)
+            expect(game.board[1][column2]).to eq(player1.symbol)
             game.print_board
 
             expect(game.check_win).to eq(true)
@@ -121,15 +129,14 @@ describe Game do
         let (:row1) {1}
         let (:row2) {2}
         let (:row3) {3}
+        let (:row4) {4}
         let (:column2) {2}
         let (:column3) {3}
         let (:column4) {4}
 
         it "p1 has 4 in a column" do
-            
-            game.place_token(player1,column2)
-            expect(game.board[row0][column2]).to eq(player1.symbol)
-            
+            game.place_token(player2,column2)
+            expect(game.board[row0][column2]).to eq(player2.symbol)
             game.place_token(player1,column2)
             expect(game.board[row1][column2]).to eq(player1.symbol)
             
@@ -138,6 +145,9 @@ describe Game do
             
             game.place_token(player1,column2)
             expect(game.board[row3][column2]).to eq(player1.symbol)
+            
+            game.place_token(player1,column2)
+            expect(game.board[row4][column2]).to eq(player1.symbol)
             game.print_board
 
             expect(game.check_win).to eq(true)
@@ -201,6 +211,114 @@ describe Game do
         let (:column4) {4}        
         let (:column5) {5}
 
+        it "p1 has 4 in a +1,+1 diagonal" do
+            game.place_token(player1,column0)
+            expect(game.board[row0][column0]).to eq(player1.symbol)
+            game.place_token(player2,column1)
+            expect(game.board[row0][column1]).to eq(player2.symbol)
+            game.place_token(player1,column1)
+            expect(game.board[row1][column1]).to eq(player1.symbol)
+            game.place_token(player2,column2)
+            expect(game.board[row0][column2]).to eq(player2.symbol)
+            game.place_token(player2,column2)
+            expect(game.board[row1][column2]).to eq(player2.symbol)
+            game.place_token(player1,column2)
+            expect(game.board[row2][column2]).to eq(player1.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[row0][column3]).to eq(player2.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[row1][column3]).to eq(player2.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[row2][column3]).to eq(player2.symbol)
+            game.place_token(player1,column3)
+            expect(game.board[row3][column3]).to eq(player1.symbol)
+            expect(game.check_win).to eq(true)
+            game.print_board
+
+        end
+
+        it "Diagonal pattern: p1,p1,p2,p1" do
+            game.place_token(player1,column0)
+            expect(game.board[row0][column0]).to eq(player1.symbol)
+            game.place_token(player2,column1)
+            expect(game.board[row0][column1]).to eq(player2.symbol)
+            game.place_token(player1,column1)
+            expect(game.board[row1][column1]).to eq(player1.symbol)
+            game.place_token(player2,column2)
+            expect(game.board[row0][column2]).to eq(player2.symbol)
+            game.place_token(player2,column2)
+            expect(game.board[row1][column2]).to eq(player2.symbol)
+            game.place_token(player2,column2)
+            expect(game.board[row2][column2]).to eq(player2.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[row0][column3]).to eq(player2.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[row1][column3]).to eq(player2.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[row2][column3]).to eq(player2.symbol)
+            game.place_token(player1,column3)
+            expect(game.board[row3][column3]).to eq(player1.symbol)
+            expect(game.check_win).to eq(false)
+            game.print_board
+
+        end
+
+        it "p2 has 4 in a  +1 ,1diagonal" do
+            game.place_token(player2,column0)
+            expect(game.board[row0][column0]).to eq(player2.symbol)
+            game.place_token(player1,column1)
+            expect(game.board[row0][column1]).to eq(player1.symbol)
+            game.place_token(player2,column1)
+            expect(game.board[row1][column1]).to eq(player2.symbol)
+            game.place_token(player1,column2)
+            expect(game.board[row0][column2]).to eq(player1.symbol)
+            game.place_token(player1,column2)
+            expect(game.board[row1][column2]).to eq(player1.symbol)
+            game.place_token(player2,column2)
+            expect(game.board[row2][column2]).to eq(player2.symbol)
+            game.place_token(player1,column3)
+            expect(game.board[row0][column3]).to eq(player1.symbol)
+            game.place_token(player1,column3)
+            expect(game.board[row1][column3]).to eq(player1.symbol)
+            game.place_token(player1,column3)
+            expect(game.board[row2][column3]).to eq(player1.symbol)
+            game.place_token(player2,column3)
+            expect(game.board[row3][column3]).to eq(player2.symbol)
+            expect(game.check_win).to eq(true)
+            game.print_board
+
+        end
+
+        it "p1 has 4 in a +1,-1 diagonal" do
+            game.place_token(player2,column1)
+            expect(game.board[row0][column1]).to eq(player2.symbol)
+            game.place_token(player2,column1)
+            expect(game.board[row1][column1]).to eq(player2.symbol)
+            game.place_token(player1,column1)
+            expect(game.board[row2][column1]).to eq(player1.symbol)
+            game.place_token(player1,column1)
+            expect(game.board[row3][column1]).to eq(player1.symbol)
+
+            game.place_token(player1,column2)
+            expect(game.board[row0][column2]).to eq(player1.symbol)
+            game.place_token(player2,column2)
+            expect(game.board[row1][column2]).to eq(player2.symbol)
+            game.place_token(player1,column2)
+            expect(game.board[row2][column2]).to eq(player1.symbol)
+
+            game.place_token(player1,column3)
+            expect(game.board[row0][column3]).to eq(player1.symbol)
+            game.place_token(player1,column3)
+            expect(game.board[row1][column3]).to eq(player1.symbol)
+
+            game.place_token(player1,column4)
+            expect(game.board[row0][column4]).to eq(player1.symbol)
+            game.print_board
+
+            expect(game.check_win).to eq(true)
+
+
+        end
         
 
     end
